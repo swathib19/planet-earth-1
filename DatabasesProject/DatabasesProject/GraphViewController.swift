c77e334312809538f9df09f295e9c73bd4e8f63a
rnb11 <rnb11@duke.edu>
2017-12-09 15:27:11 -0500

    graph page

---
+++ DatabasesProject/DatabasesProject/GraphViewController.swift
@@ -0,0 +1,21 @@
+//
+//  GraphViewController.swift
+//  DatabasesProject
+//
+//  Created by Rachel Bransom on 12/9/17.
+//  Copyright © 2017 Tahia Emran. All rights reserved.
+//
+
+import UIKit
+
+class GraphViewController: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
