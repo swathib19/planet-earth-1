210741bd47a3eacae7a2f3119c073096ae61b877
rnb11 <rnb11@duke.edu>
2017-12-09 17:10:02 -0500

    fixed graph

--- DatabasesProject/DatabasesProject/GraphView.swift
+++ DatabasesProject/DatabasesProject/GraphView.swift
@@ -17,5 +17,149 @@ class GraphView: UIView {
         // Drawing code
     }
     */
+    
+//    let darkPurple = UIColor(red: 17.0/255.0, green: 29.0/255.0, blue: 68.0/255.0, alpha: 1.0)
+//    let lightPurple = UIColor(red: 48.0/255.0, green: 59.0/255.0, blue: 116.0/255.0, alpha: 1.0)
+//    let lightPurple1 = UIColor(red: 75.0/255.0, green: 102.0/255.0, blue: 160.0/255.0, alpha: 1.0)
+//    let middle =  UIColor(red: 90.0/255.0, green: 132.0/255.0, blue: 182.0/255.0, alpha: 1.0)
+//    let blue1 =  UIColor(red: 103.0/255.0, green: 159.0/255.0, blue: 202.0/255.0, alpha: 1.0)
+//    let darkBlue = UIColor(red: 125.0/255.0, green: 203.0/255.0, blue: 232.0/255.0, alpha: 1.0)
+//
+    
+    private struct Constants {
+        static let cornerRadiusSize = CGSize(width: 8.0, height: 8.0)
+        static let margin: CGFloat = 20.0
+        static let topBorder: CGFloat = 60
+        static let bottomBorder: CGFloat = 50
+        static let colorAlpha: CGFloat = 0.3
+        static let circleDiameter: CGFloat = 5.0
+    }
+    
+    var graphPoints = [4, 2, 6, 4, 5, 8, 3]
+    // 1
+    @IBInspectable var startColor: UIColor = .white
+    @IBInspectable var endColor: UIColor = .clear
+    
+    convenience init(){
+//        self.init(frame: CGRect.zero)
+//        let newLayer = CAGradientLayer()
+//        newLayer.colors = [darkPurple.cgColor, lightPurple.cgColor, lightPurple1.cgColor, middle.cgColor  ,blue1.cgColor ,darkBlue.cgColor]
+//        newLayer.frame = self.frame
+//        self.layer.insertSublayer(newLayer, at: 0)
+//       // self.insertSublayer(newLayer, at: 0)
+        self.init(frame: CGRect.zero)
+        self.backgroundColor = UIColor.clear
+    }
+    
+    override func draw(_ rect: CGRect) {
+        
+        let width = rect.width
+        let height = rect.height
+        // 2
+        
+        let context = UIGraphicsGetCurrentContext()!
+        
+        let colors = [startColor.cgColor, endColor.cgColor]
+        
+        // 3
+        let colorSpace = CGColorSpaceCreateDeviceRGB()
+        
+        // 4
+        let colorLocations: [CGFloat] = [0.0, 1.0]
+        
+        let gradient = CGGradient(colorsSpace: colorSpace,
+                                  colors: colors as CFArray,
+                                  locations: colorLocations)!
+        
+        let startPoint = CGPoint.zero
+        let endPoint = CGPoint(x: 0, y: bounds.height)
+        
+        
+        let margin = Constants.margin
+        let graphWidth = width - margin * 2 - 4
+        let columnXPoint = { (column: Int) -> CGFloat in
+            //Calculate the gap between points
+            let spacing = graphWidth / CGFloat(self.graphPoints.count - 1)
+            return CGFloat(column) * spacing + margin + 2
+        }
+        
+        let topBorder = Constants.topBorder
+        let bottomBorder = Constants.bottomBorder
+        let graphHeight = height - topBorder - bottomBorder
+        let maxValue = graphPoints.max()!
+        let columnYPoint = { (graphPoint: Int) -> CGFloat in
+            let y = CGFloat(graphPoint) / CGFloat(maxValue) * graphHeight
+            return graphHeight + topBorder - y // Flip the graph
+        }
+        
+        UIColor.white.setFill()
+        UIColor.white.setStroke()
+        
+        // set up the points line
+        let graphPath = UIBezierPath()
+        
+        // go to start of line
+        graphPath.move(to: CGPoint(x: columnXPoint(0), y: columnYPoint(graphPoints[0])))
+        
+        // add points for each item in the graphPoints array
+        // at the correct (x, y) for the point
+        for i in 1..<graphPoints.count {
+            let nextPoint = CGPoint(x: columnXPoint(i), y: columnYPoint(graphPoints[i]))
+            graphPath.addLine(to: nextPoint)
+        }
+        
+        //Create the clipping path for the graph gradient
+        
+        //1 - save the state of the context (commented out for now)
+        //context.saveGState()
+        
+        //2 - make a copy of the path
+        let clippingPath = graphPath.copy() as! UIBezierPath
+        
+        //3 - add lines to the copied path to complete the clip area
+        clippingPath.addLine(to: CGPoint(x: columnXPoint(graphPoints.count - 1), y:height))
+        clippingPath.addLine(to: CGPoint(x:columnXPoint(0), y:height))
+        clippingPath.close()
+        
+        //4 - add the clipping path to the context
+        clippingPath.addClip()
+        
+        let highestYPoint = columnYPoint(maxValue)
+        let graphStartPoint = CGPoint(x: margin, y: highestYPoint)
+        let graphEndPoint = CGPoint(x: margin, y: bounds.height)
+        
+        context.drawLinearGradient(gradient, start: graphStartPoint, end: graphEndPoint, options: [])
+        graphPath.lineWidth = 2.0
+        graphPath.stroke()
+        
+        for i in 0..<graphPoints.count {
+            var point = CGPoint(x: columnXPoint(i), y: columnYPoint(graphPoints[i]))
+            point.x -= Constants.circleDiameter / 2
+            point.y -= Constants.circleDiameter / 2
+            
+            let circle = UIBezierPath(ovalIn: CGRect(origin: point, size: CGSize(width: Constants.circleDiameter, height: Constants.circleDiameter)))
+            circle.fill()
+            
+        }
+        
+        let linePath = UIBezierPath()
+        
+        //top line
+        linePath.move(to: CGPoint(x: margin, y: topBorder))
+        linePath.addLine(to: CGPoint(x: width - margin, y: topBorder))
+        
+        //center line
+        linePath.move(to: CGPoint(x: margin, y: graphHeight/2 + topBorder))
+        linePath.addLine(to: CGPoint(x: width - margin, y: graphHeight/2 + topBorder))
+        
+        //bottom line
+        linePath.move(to: CGPoint(x: margin, y:height - bottomBorder))
+        linePath.addLine(to: CGPoint(x:  width - margin, y: height - bottomBorder))
+        let color = UIColor(white: 1.0, alpha: Constants.colorAlpha)
+        color.setStroke()
+        
+        linePath.lineWidth = 1.0
+        linePath.stroke()
+}
 
 }
--- DatabasesProject/DatabasesProject/GraphViewController.swift
+++ DatabasesProject/DatabasesProject/GraphViewController.swift
@@ -9,6 +9,14 @@
 import UIKit
 
 class GraphViewController: UIViewController {
+    
+   // @IBOutlet weak var graphView: GraphView!
+//    @IBOutlet weak var averageWaterDrunk: UILabel!
+//    @IBOutlet weak var maxLabel: UILabel!
+//    @IBOutlet weak var stackView: UIStackView!
+    
+    var graph: GraphContainer!
+    
     let darkPurple = UIColor(red: 17.0/255.0, green: 29.0/255.0, blue: 68.0/255.0, alpha: 1.0)
     let lightPurple = UIColor(red: 48.0/255.0, green: 59.0/255.0, blue: 116.0/255.0, alpha: 1.0)
     let lightPurple1 = UIColor(red: 75.0/255.0, green: 102.0/255.0, blue: 160.0/255.0, alpha: 1.0)
@@ -22,6 +30,14 @@ class GraphViewController: UIViewController {
         newLayer.colors = [darkPurple.cgColor, lightPurple.cgColor, lightPurple1.cgColor, middle.cgColor  ,blue1.cgColor ,darkBlue.cgColor]
         newLayer.frame = self.view.frame
         view.layer.insertSublayer(newLayer, at: 0)
+     
+        
+        self.graph = GraphContainer()
+        self.view.addSubview(self.graph)
+        self.graph.height(h: 300)
+        self.graph.width(w: 300)
+        self.graph.pinTop(v: self.view, o: 100)
+        self.graph.pinLeft(v: self.view, o: 50)
         // Do any additional setup after loading the view.
     }
 
@@ -30,7 +46,35 @@ class GraphViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+//    func setupGraphDisplay() {
+//
+//      //  let maxDayIndex = stackView.arrangedSubviews.count - 1
+//
+//        //  1 - replace last day with today's actual data
+//        //graphView.graphPoints[graphView.graphPoints.count - 1] = counterView.counter
+//        //2 - indicate that the graph needs to be redrawn
+//        graphView.setNeedsDisplay()
+//      //  maxLabel.text = "\(graphView.graphPoints.max()!)"
+//
+//        //  3 - calculate average from graphPoints
+//        let average = graphView.graphPoints.reduce(0, +) / graphView.graphPoints.count
+//     //   averageWaterDrunk.text = "\(average)"
+//
+//        // 4 - setup date formatter and calendar
+//        let today = Date()
+//        let calendar = Calendar.current
+//
+//        let formatter = DateFormatter()
+//        formatter.setLocalizedDateFormatFromTemplate("EEEEE")
+//
+//        // 5 - set up the day name labels with correct days
+//        for i in 0...maxDayIndex {
+//            if let date = calendar.date(byAdding: .day, value: -i, to: today),
+//                let label = stackView.arrangedSubviews[maxDayIndex - i] as? UILabel {
+//                label.text = formatter.string(from: date)
+//            }
+//        }
+//    }
     /*
     // MARK: - Navigation
 
