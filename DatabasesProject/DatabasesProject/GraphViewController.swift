288db10a22feba000367880fb1a48b44d352ead7
Swathi Balakrishnan <SwathiB@Swathis-MacBook-Pro.local>
2017-12-09 15:34:38 -0500

    background gradient on graph page

--- DatabasesProject/DatabasesProject/GraphViewController.swift
+++ DatabasesProject/DatabasesProject/GraphViewController.swift
@@ -9,10 +9,19 @@
 import UIKit
 
 class GraphViewController: UIViewController {
-
+    let darkPurple = UIColor(red: 17.0/255.0, green: 29.0/255.0, blue: 68.0/255.0, alpha: 1.0)
+    let lightPurple = UIColor(red: 48.0/255.0, green: 59.0/255.0, blue: 116.0/255.0, alpha: 1.0)
+    let lightPurple1 = UIColor(red: 75.0/255.0, green: 102.0/255.0, blue: 160.0/255.0, alpha: 1.0)
+    let middle =  UIColor(red: 90.0/255.0, green: 132.0/255.0, blue: 182.0/255.0, alpha: 1.0)
+    let blue1 =  UIColor(red: 103.0/255.0, green: 159.0/255.0, blue: 202.0/255.0, alpha: 1.0)
+    let darkBlue = UIColor(red: 125.0/255.0, green: 203.0/255.0, blue: 232.0/255.0, alpha: 1.0)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        let newLayer = CAGradientLayer()
+        newLayer.colors = [darkPurple.cgColor, lightPurple.cgColor, lightPurple1.cgColor, middle.cgColor  ,blue1.cgColor ,darkBlue.cgColor]
+        newLayer.frame = self.view.frame
+        view.layer.insertSublayer(newLayer, at: 0)
         // Do any additional setup after loading the view.
     }
 
